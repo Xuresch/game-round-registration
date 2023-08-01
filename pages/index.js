f71b0f5f315693f42a8ccad8f0506b69dc084965
Niko Rauner <niko.rauner@googlemail.com>
2023-08-01 05:13:24 +0200

    style(app): change quotes

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import styles from '@/styles/Home.module.css'
+import styles from "@/styles/Home.module.css";
 
 function HomePage() {
   return (
     <div>
       <p>Home Page</p>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
